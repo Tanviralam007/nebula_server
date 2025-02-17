1b0e5da1ad7a062c5fcf4bb06983605ceec67a60
Tanviralam007 <tanviralam451@gmail.com>
2025-02-17 19:47:13 +0600

    added error handers

--- src/server.cpp
+++ src/server.cpp
@@ -7,36 +7,77 @@
 #include <WS2tcpip.h>
 #include <fstream>
 
+void clean_socket(SOCKET sock){
+    closesocket(sock);
+    WSACleanup();
+}
 
 int main(int argc, char** argv){
     WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-    SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+        std::cerr << "Error: WSAStartup failed with error code: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+    
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == INVALID_SOCKET){
+        std::cerr << "Error: socket failed with error code: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
 
     // binding the socket to the port 8080
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(8080);
     addr.sin_addr.s_addr = 0;
-    bind(s, (sockaddr*)&addr, sizeof(addr));
+
+    if(bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR){
+        std::cerr << "Error: bind failed with error code: " << WSAGetLastError() << std::endl;
+        clean_socket(sock);
+        return 1;
+    }
 
     // listening for connections
-    listen(s, 10);
+    int result = listen(sock, 50);
+    if(result == SOCKET_ERROR){
+        std::cerr << "Error: listen failed with error code: " << WSAGetLastError() << std::endl;
+        clean_socket(sock);
+        return 1;
+    }
 
     // accepting a connection
-    SOCKET client = accept(s, NULL, NULL);
+    SOCKET client = accept(sock, NULL, NULL);
+    if(client == INVALID_SOCKET){
+        std::cerr << "Error: accept failed with error code: " << WSAGetLastError() << std::endl;
+        clean_socket(sock);
+        return 1;
+    }
 
     // receive
-    char request[256] = {0};
-    recv(client, request, 256, 0);
+    char request[4096] = {0};
+    int byte_received = recv(client, request, 4096, 0);
+    if(byte_received == SOCKET_ERROR){
+        std::cerr << "Error: recv failed with error code: " << WSAGetLastError() << std::endl;
+        clean_socket(sock);
+        return 1;
+    }
 
     // send
     if(memcmp(request, "GET / HTTP/1.1", 6) == 0){
         FILE* f = fopen("../views/index.html", "r");
-        char buffer[256] = {0};
-        fread(buffer, 1, 256, f);
-        send(client, buffer, 256, 0);
+        if(!f){
+            std::cerr << "Error: Could not open index.html" << std::endl;
+            const char *error_404 = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(client, error_404, strlen(error_404), 0);
+        }
+        char buffer[4096] = {0};
+        fread(buffer, 1, 4096, f);
+        send(client, buffer, 4096, 0);
     }
 
+    closesocket(client);
+    clean_socket(sock);
     return 0;
 }
